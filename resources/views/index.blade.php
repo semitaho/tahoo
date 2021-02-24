be3b9e3ae3d46514d7c6014de91dfa54ff06552c
Aho Toni <ext.toni.aho@ruokavirasto.fi>
2021-02-24 17:41:27 +0200

    jees

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
 @section('content')
 
     <h2>Yritykseni</h2>
-    <p>Taho Ohjelmistopalvelut Oy on 2018 perustamani ohjelmistokonsultointiyritys. Myyn yksittäistä suunnittelu- ja
+    <p>Taho Ohjelmistopalvelut Oy on 2018 perustamani ohjelmistokonsultointiyritys. Myyn yksittäistä suunnittelu-, arkkitehti- ja
         kehitysosaamista
         asiakkaitteni ohjelmistoprojekteihin <em>laadukkaasti</em> ja <em>tehokkaasti</em> yli 10 vuoden kokemuksella.</p>
 
@@ -25,10 +25,23 @@
 
 
     <h2 class="margin-spacer">Omat projektit</h2>
-    <p>Jokainen intohimoinen koodari harrastaa omia projektejaan. Niin myös minäkin. Esimerkkinä <a target="_blank"
-                                                                                                    href="https://www.superlender.fi/">SuperLender</a>-vertaislainasovellus,
-        jonka
-        toteutin täysin itsenäisesti. Ja mikä parasta, sain valita uusimmat mahdolliset teknologiat, <em>Angular 5</em> ja <em>Clojure</em>.
+
+    <p>Jokainen intohimoinen koodari harrastaa omia projektejaan. Alla pari esimerkkiä, mitä olen toteuttanut.</p>
+    <h3>Taho-forms</h3>
+    <p>Angular 5 -teknologialla toteutettu lomakemoottorikirjasto, jonka toteutin vuonna 2019 nykyiseen julkishallinnon projektiin.
+    Kirjaston avulla yhtenäistettiin eri lomakkeiden käyttö kyseisessä järjestelmässä. </p>
+
+    <p>Kirjasto toimii siten, että komponentille määritellään <i>deklaratiivisesti</i> JSON-olio, joka generoi käyttöliittymään määritteiden mukaisen lomakkeen.
+       Kyseinen kirjasto sisältää myös <i>lomake-editorin</i> esim. substanssin ylläpitoa varten. Kirjasto on helposti laajennettavissa erilaisia <i>teemoja</i> varten. 
+
+    <p>Kirjaston avulla on todella helppo generoida ja integroida toimivia, dynaamisia lomakkeita olemassa olevaan tietojärjestelmän kehitysprojektiin. Tämä puolestaan parantaa 
+    tuottavuutta, koska varsinaisia käyttöliittymälomakkeita ei itse tarvitse toteuttaa. Laaja kustomointi mahdollistaa sen, että kirjaston voi räätälöidä nimenomaan oman projektin tarpeisiin.</p>
+    <p>Jos aloit kiinnostumaan, kysy minulta <a href="#yhteystiedot">lisää!</a></p> 
+    <h3>SuperLender</h3>
+    <p>Toteutin <a target="_blank"
+                                                                                                    href="https://www.superlender.fi/">SuperLender</a>-vertaislainasovelluksen
+    
+     täysin itsenäisesti alusta tuotantoon. Ja mikä parasta, sain valita uusimmat mahdolliset teknologiat, <em>Angular 5</em> ja <em>Clojure</em>.
     </p>
 
     <p>Voit myös tutustua muihin projekteihini <a href="https://github.com/semitaho">GitHub</a>-sivustoltani.</p>
@@ -37,7 +50,7 @@
         lopputuloksen. Intohimoni on toimia itsenäisesti tietyn kokonaisuuden parissa tuloksekkaasti. Tästä olen saanut kiitosta
         lukuisilta asiakkailtani ollessani palkkatöissä.</p>
     <h2 class="margin-spacer">Kiinnostuitko?</h2>
-    <p>Ota yhteyttä, niin sovitaan toimeksianto!</p>
+    <p id="yhteystiedot">Ota yhteyttä, niin sovitaan toimeksianto!</p>
     <img class="cv-image" src="{{asset('/resources/assets/images/taho.jpg')}}">
     @include('contact-address')
 
